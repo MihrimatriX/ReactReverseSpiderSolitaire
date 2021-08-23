28afef1360700d7ed345c53bb8ad402e7a45a13c
Ahmet Faruk Uzunkaya <sosyal.ahmet.fuzunkaya@gmail.com>
2021-08-24 00:11:09 +0300

    sytle fix

--- src/components/modules/Header/Header.js
+++ src/components/modules/Header/Header.js
@@ -5,7 +5,7 @@ import styles from "./Header.module.css";
 function Header({ completed, moveCount }) {
   return (
     <div className={styles.header}>
-      <button onClick={() => window.location.reload()}>New Game</button>
+      <button className={styles.btn} onClick={() => window.location.reload()}>New Game</button>
       <span>
         Completed: {completed} Move:{moveCount}
       </span>
